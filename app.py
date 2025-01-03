704c11d45b1da064d980e2b1b89600973d28de06
agentjimlam <jlam.5883@gmail.com>
2025-01-04 01:58:34 +0800

    assignment done, practiced docker build image and Re-tag existing image, then docker push the image to docker hub repo. Also practiced docker run -d -p 3000:3000 <image-id> and docker stop <container-id>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from Flask & Docker. My name is Jimmy! version 1.2.0</h2>'
 
 
 if __name__ == "__main__":
